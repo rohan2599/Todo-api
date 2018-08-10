af10c072648e9e611a1c49a9187eb9cb329df3ff
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-08-10 15:09:45 +0530

    added delete file scripts in mongo

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -14,7 +14,28 @@ MongoClient.connect('mongodb://localhost:27017/ToDoApp',(err,db)=>{
   
 	console.log('connected successfully');
 
+	//deleteMany
+	db.collection('todos').deleteMany({text:'hey'}).then((result)=>{
+		console.log(result);
+	})
+
+
+	//deleteOne
+
+	 db.collection('todos').deleteOne({text:'helo'}).then((result)=>{
+	 	console.log(result);
+	 }); 
+
+	//findOneAndDelete
+	db.collection('todos').findOneAndDelete({id:12344}).then((result)=>{
+		console.log(result);
+	})
+
+	//findOneAndDelete By Id
 	
+	// db.collection('todos').findOneAndDelete({_id: new ObjectId('5b6d5c5d2f127f2b4d5ad064')}).then((reply)=>{
+	// 	console.log(reply);
+	// })
 
 
 	db.close();

e3de642128126507921af70a0d679b423bb7d0fe
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-09-07 23:11:46 +0530

    added dynamic id routing

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');  
+var {ObjectId} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -33,6 +34,51 @@ app.get('/todos',(req,res)=>{
 		res.status(400).send(err);
 	})
 })
+
+
+
+
+
+
+// app.get('/todos/:id',(req,res)=>{
+// 	var id = req.params.id;
+// 	if(!ObejctId.isValid(id)){
+// 		return res.status(404).send();
+// 	}
+
+// 	Todo.findById(id).then((todo)=>{
+// 		if(!todo){
+// 			return res.status(404).send();
+// 		}
+// 		res.status(200).send({todo});
+// 	})
+// },(e)=>{
+// 	res.status(404).send();
+// });
+
+
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
+
+
 
 
 app.listen(3000,()=>{

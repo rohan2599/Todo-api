f5caf35c1a23ef6ea1f3436a81f56d5c5c3735e7
Rohan2599 <201651044@iiitvadodara.ac.in>
2018-09-12 01:56:05 +0530

    added PATCH todos in server file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');  
 var {ObjectId} = require('mongodb');
+var  _ = require('lodash');  
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -17,7 +18,7 @@ app.use(bodyParser.json());
 app.post('/todos',(req,res)=>{
 	var todo = new Todo({
 		text:req.body.text
-	});
+	}); 
 
 	todo.save().then((doc)=>{
 		res.send(doc);
@@ -92,10 +93,36 @@ app.delete('/todos/:id',(req,res)=>{
 			}
 			res.send({todo: todo});
 	}).catch((e)=>{
-		res.status(400).send(e);
+		res.status(400).send();
 	});
 });
 
+
+app.patch('/todos/:id',(req,res)=>{
+	var id = req.params.id;
+	var body = _.pick(req.body,['text','flag']);
+	if(!ObjectId.isValid(id)){
+		return res.status(404).send();
+
+	}
+	if(_.isBoolean(body.flag) && body.flag){
+
+		body.completedAt = new Date().getTime();
+
+	}
+	else{
+			body.flag = false;
+			body.completedAt=null;
+	}
+Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+	if(!todo){
+		return res.status(404).send();
+	}
+
+	res.send({todo});
+}).catch((e)=>{ res.status(400).send()})
+
+});
 
 app.listen(	port,()=>{
 	console.log(`started on port ${port}`);
